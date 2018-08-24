4ce611b16f0b94af44ea45d359ac6bdd9ff64007
iadfeshchm <zlatov@ya.ru>
2018-08-24 12:53:19 +0300

    add automatic_leafing_over and fix outing of the panel outside the window with position relative

--- dist/zlader.js
+++ dist/zlader.js
@@ -37,6 +37,29 @@
     return instance
   }
 
+  // Приватные методы не доступные для jQ объекта
+  var method = {
+    activate_relays: function() {
+      this.on('click', this.options.selectors.relay, {instance: this}, method.relay_click_handler)
+    },
+    activate_automatic_leafing_over: function() {
+      this.activate_leafing_over()
+      method.activate_leafing_over_pauses.call(this)
+    },
+    relay_click_handler: function(event) {
+      var instance = event.data.instance
+      var active_slide_index = instance.get_active_slide_index()
+      var relay_index = instance.relays.index($(this))
+      if (active_slide_index != relay_index) {
+        instance.view(relay_index)
+      }
+    },
+    activate_leafing_over_pauses: function() {
+      this.on('mouseenter', this.deactivate_leafing_over.bind(this))
+      this.on('mouseleave', this.activate_leafing_over.bind(this))
+    }
+  }
+
   $.fn.zlader._initialize = function(o) {
     this.options = $.extend($.fn.zlader.default_options, o || {});
     this.frame = this.find(this.options.selectors.frame).first()
@@ -62,22 +85,13 @@
       this.relays.eq(this.get_active_slide_index()).addClass("active")
     }
     this.statuses = {
-      is_animated: false
+      is_animated: false,
+      leafing_over_interval_id: null
     }
+
     this.activate = function() {
-      this.activate_relays()
-      this.activate_interval()
-    }
-    this.activate_relays = function() {
-      this.on('click', this.options.selectors.relay, {instance: this}, this.relay_click_handler)
-    }
-    this.relay_click_handler = function(event) {
-      var instance = event.data.instance
-      var active_slide_index = instance.get_active_slide_index()
-      var relay_index = instance.relays.index($(this))
-      if (active_slide_index != relay_index) {
-        instance.view(relay_index)
-      }
+      method.activate_relays.call(this)
+      method.activate_automatic_leafing_over.call(this)
     }
     this.view = function(index) {
       if (this.statuses.is_animated) {
@@ -102,7 +116,23 @@
         instance.statuses.is_animated = false
       }})
     }
-    this.activate_interval = function() {
+    this.activate_leafing_over = function() {
+      if (
+        this.statuses.leafing_over_interval_id != null ||
+        this.options.leafing_over_interval == null || this.options.leafing_over_interval == 0
+      ) {
+        return null
+      }
+      this.statuses.leafing_over_interval_id = setInterval(this.next.bind(this), this.options.leafing_over_interval)
+    }
+    this.deactivate_leafing_over = function() {
+      clearInterval(this.statuses.leafing_over_interval_id)
+      this.statuses.leafing_over_interval_id = null
+    }
+    this.next = function() {
+      var active_slide_index = this.get_active_slide_index()
+      var next_index = (active_slide_index == this.slides.length - 1) ? 0 : ++active_slide_index
+      this.view(next_index)
     }
     return this
   }
@@ -113,7 +143,8 @@
       relay: '.relay',
       frame: '.frame',
       slide: '.slide'
-    }
+    },
+    leafing_over_interval: 8000
   }
 
 })(jQuery);
